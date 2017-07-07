2842332e5a2bec04f8ef168b7f8e1e3a36a1f81d
autodocs <autodocs>
2017-07-07 04:59:06 +0000

    build based on 9860a37

--- latest/search_index.js
+++ latest/search_index.js
@@ -313,6 +313,22 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "examples.html#",
+    "page": "Examples",
+    "title": "Examples",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "examples.html#Examples-1",
+    "page": "Examples",
+    "title": "Examples",
+    "category": "section",
+    "text": "A set of examples in the form of Jupyter notebook is made available in the examples folder of the project. They are distributed with GeoStats.jl and can be run locally for full interactivity.We are always happy to include new use cases. Please check Contributing before submitting a pull request."
+},
+
+{
     "location": "library.html#",
     "page": "Library",
     "title": "Library",
