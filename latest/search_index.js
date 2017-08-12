71749c901344fe6f40ab1fa3568eef1f5ff5ff5e
autodocs <autodocs>
2017-08-12 18:33:35 +0000

    build based on 409f61b

--- latest/search_index.js
+++ latest/search_index.js
@@ -233,104 +233,104 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "estimation.html#",
-    "page": "Kriging estimation",
-    "title": "Kriging estimation",
+    "location": "estimators.html#",
+    "page": "Kriging estimators",
+    "title": "Kriging estimators",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "estimation.html#GeoStats.estimate",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.estimate",
+    "page": "Kriging estimators",
     "title": "GeoStats.estimate",
     "category": "Function",
     "text": "estimate(estimator, xₒ)\n\nCompute mean and variance for the estimator at location xₒ.\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#GeoStats.fit!",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.fit!",
+    "page": "Kriging estimators",
     "title": "GeoStats.fit!",
     "category": "Function",
     "text": "fit!(estimator, X, z)\n\nBuild Kriging system from locations X with values z and save factorization in estimator.\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#GeoStats.weights",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.weights",
+    "page": "Kriging estimators",
     "title": "GeoStats.weights",
     "category": "Function",
     "text": "weights(estimator, xₒ)\n\nCompute the weights λ (and Lagrange multipliers ν) for the estimator at location xₒ.\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#Kriging-estimation-1",
-    "page": "Kriging estimation",
-    "title": "Kriging estimation",
+    "location": "estimators.html#Kriging-estimators-1",
+    "page": "Kriging estimators",
+    "title": "Kriging estimators",
     "category": "section",
     "text": "A Kriging estimator has the form:newcommandxboldsymbolx\nnewcommandRmathbbR\nhatZ(x_0) = lambda_1 Z(x_1) + lambda_2 Z(x_2) + cdots + lambda_n Z(x_n)quad x_i in R^m lambda_i in Rwith Zcolon R^m times Omega to R a random field.This package implements the following Kriging variants:Simple Kriging\nOrdinary Kriging\nUniversal Kriging\nExternal Drift KrigingAll these variants follow the same interface: an estimator object is first created with a given data configuration and variogram model, and then estimates are made at various locations.The object construction takes care of building the Kriging system and factorizing the LHS with an appropriate decomposition (e.g. Cholesky, LU). The estimate method performs the estimation at a given location:estimateA typical use of the interface is as follows:# build and factorize the system\nsimkrig = SimpleKriging(X, z, γ, mean(z))\n\n# estimate at various locations\nfor xₒ in locations\n  μ, σ² = estimate(simkrig, xₒ)\nendIn case the data configuration needs to be changed in a loop (e.g. sequential Gaussian simulation), one can keep all the parameters fixed and only update the factorization with the fit! method:fit!For advanced users, the Kriging weights and Lagrange multipliers at a given location can be accessed with the weights method. This method returns an AbstractWeights object containing a field λ for the weights and a field ν for the Lagrange multipliers:weights# weights and Lagrange multipliers\nOKweights = weights(ordkrig, xₒ)\nOKweights.λ, OKweights.ν"
 },
 
 {
-    "location": "estimation.html#GeoStats.SimpleKriging",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.SimpleKriging",
+    "page": "Kriging estimators",
     "title": "GeoStats.SimpleKriging",
     "category": "Type",
     "text": "SimpleKriging(X, z, γ, μ)\n\nParameters\n\nX ∈ ℜ^(mxn) - matrix of data locations\nz ∈ ℜⁿ      - vector of observations for X\nγ           - variogram model\nμ ∈ ℜ       - mean of z\n\nNotes\n\nSimple Kriging requires stationary variograms\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#Simple-Kriging-1",
-    "page": "Kriging estimation",
+    "location": "estimators.html#Simple-Kriging-1",
+    "page": "Kriging estimators",
     "title": "Simple Kriging",
     "category": "section",
     "text": "In Simple Kriging, the mean mu of the random field is assumed to be constant and known. The resulting linear system is:newcommandCboldsymbolC\nnewcommandcboldsymbolc\nnewcommandlboldsymbollambda\nnewcommand1boldsymbol1\nnewcommandzboldsymbolz\nbeginbmatrix\ncov(x_1x_2)  cov(x_1x_2)  cdots  cov(x_1x_n) \ncov(x_2x_1)  cov(x_2x_2)  cdots  cov(x_2x_n) \nvdots  vdots  ddots  vdots \ncov(x_nx_1)  cov(x_nx_2)  cdots  cov(x_nx_n)\nendbmatrix\nbeginbmatrix\nlambda_1 \nlambda_2 \nvdots \nlambda_n\nendbmatrix\n=\nbeginbmatrix\ncov(x_1x_0) \ncov(x_2x_0) \nvdots \ncov(x_nx_0)\nendbmatrixor in matricial form Cl = c. We subtract the given mean from the observations boldsymboly = z - mu 1 and compute the mean and variance at location x_0:mu(x_0) = mu + boldsymboly^top lsigma^2(x_0) = cov(0) - c^top lSimpleKriging"
 },
 
 {
-    "location": "estimation.html#GeoStats.OrdinaryKriging",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.OrdinaryKriging",
+    "page": "Kriging estimators",
     "title": "GeoStats.OrdinaryKriging",
     "category": "Type",
     "text": "OrdinaryKriging(X, z, γ)\n\nParameters\n\nX ∈ ℜ^(mxn) - matrix of data locations\nz ∈ ℜⁿ      - vector of observations for X\nγ           - variogram model\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#Ordinary-Kriging-1",
-    "page": "Kriging estimation",
+    "location": "estimators.html#Ordinary-Kriging-1",
+    "page": "Kriging estimators",
     "title": "Ordinary Kriging",
     "category": "section",
     "text": "In Ordinary Kriging the mean of the random field is assumed to be constant and unknown. The resulting linear system is:newcommandGboldsymbolGamma\nnewcommandgboldsymbolgamma\nbeginbmatrix\nG  1 \n1^top  0\nendbmatrix\nbeginbmatrix\nl \nnu\nendbmatrix\n=\nbeginbmatrix\ng \n1\nendbmatrixwith nu the Lagrange multiplier associated with the constraint 1^top l = 1. The mean and variance at location x_0 are given by:mu(x_0) = z^top lambdasigma^2(x_0) =  beginbmatrix g  1 endbmatrix^top beginbmatrix l  nu endbmatrixOrdinaryKriging"
 },
 
 {
-    "location": "estimation.html#GeoStats.UniversalKriging",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.UniversalKriging",
+    "page": "Kriging estimators",
     "title": "GeoStats.UniversalKriging",
     "category": "Type",
     "text": "UniversalKriging(X, z, γ, degree)\n\nParameters\n\nX ∈ ℜ^(mxn) - matrix of data locations\nz ∈ ℜⁿ      - vector of observations for X\nγ           - variogram model\ndegree      - polynomial degree for the mean\n\nNotes\n\nOrdinaryKriging is recovered for 0th degree polynomial\nFor non-polynomial mean, see ExternalDriftKriging\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#Universal-Kriging-1",
-    "page": "Kriging estimation",
+    "location": "estimators.html#Universal-Kriging-1",
+    "page": "Kriging estimators",
     "title": "Universal Kriging",
     "category": "section",
     "text": "In Universal Kriging, the mean of the random field is assumed to be a polynomial of the spatial coordinates:mu(x) = sum_k=1^N_d beta_k f_k(x)with N_d monomials f_k of degree up to d. For example, in 2D there are 6 monomials of degree up to 2:mu(x_1x_2) =  beta_1 1 + beta_2 x_1 + beta_3 x_2 + beta_4 x_1 x_2 + beta_5 x_1^2 + beta_6 x_2^2The choice of the degree d determines the size of the polynomial matrixnewcommandFboldsymbolF\nnewcommandfboldsymbolf\nF =\nbeginbmatrix\nf_1(x_1)  f_2(x_1)  cdots  f_N_d(x_1) \nf_1(x_2)  f_2(x_2)  cdots  f_N_d(x_2) \nvdots  vdots  ddots  vdots \nf_1(x_n)  f_2(x_n)  cdots  f_N_d(x_n)\nendbmatrixand polynomial vector f = beginbmatrix f_1(x_0)  f_2(x_0)  cdots  f_N_d(x_0) endbmatrix^top.The variogram determines the variogram matrix:G =\nbeginbmatrix\ngamma(x_1x_1)  gamma(x_1x_2)  cdots  gamma(x_1x_n) \ngamma(x_2x_1)  gamma(x_2x_2)  cdots  gamma(x_2x_n) \nvdots  vdots  ddots  vdots \ngamma(x_nx_1)  gamma(x_nx_2)  cdots  gamma(x_nx_n)\nendbmatrixand the variogram vector g = beginbmatrix gamma(x_1x_0)  gamma(x_2x_0)  cdots  gamma(x_nx_0) endbmatrix^top.The resulting linear system is:beginbmatrix\nG  F \nF^top  boldsymbol0\nendbmatrix\nbeginbmatrix\nl \nboldsymbolnu\nendbmatrix\n=\nbeginbmatrix\ng \nf\nendbmatrixwith boldsymbolnu the Lagrange multipliers associated with the universal constraints. The mean and variance at location x_0 are given by:mu(x_0) = z^top lsigma^2(x_0) = beginbmatrixg  fendbmatrix^top beginbmatrixl  boldsymbolnuendbmatrixUniversalKriging"
 },
 
 {
-    "location": "estimation.html#GeoStats.ExternalDriftKriging",
-    "page": "Kriging estimation",
+    "location": "estimators.html#GeoStats.ExternalDriftKriging",
+    "page": "Kriging estimators",
     "title": "GeoStats.ExternalDriftKriging",
     "category": "Type",
     "text": "ExternalDriftKriging(X, z, γ, drifts)\n\nParameters\n\nX ∈ ℜ^(mxn) - matrix of data locations\nz ∈ ℜⁿ      - vector of observations for X\nγ           - variogram model\ndrifts      - vector of external drift functions m: ℜᵐ ↦ ℜ\n\nNotes\n\nExternal drift functions should be smooth\nKriging system with external drift is often unstable\nInclude a constant drift (e.g. x->1) for unbiased estimation\nOrdinaryKriging is recovered for drifts = [x->1]\nFor polynomial mean, see UniversalKriging\n\n\n\n"
 },
 
 {
-    "location": "estimation.html#External-Drift-Kriging-1",
-    "page": "Kriging estimation",
+    "location": "estimators.html#External-Drift-Kriging-1",
+    "page": "Kriging estimators",
     "title": "External Drift Kriging",
     "category": "section",
     "text": "In External Drift Kriging, the mean of the random field is assumed to be a combination of known smooth functions:mu(x) = sum_k beta_k m_k(x)Differently than Universal Kriging, the functions m_k are not necessarily polynomials of the spatial coordinates. In practice, they represent a list of variables that is strongly correlated (and co-located) with the variable being estimated.External drifts are known to cause numerical instability. Give preference to other Kriging variants if possible.ExternalDriftKriging"
