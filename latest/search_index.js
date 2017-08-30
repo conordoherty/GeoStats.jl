878032e21741102797be0afc00f06917ce897a2d
autodocs <autodocs>
2017-08-30 23:03:26 +0000

    build based on 6111cfb

--- latest/search_index.js
+++ latest/search_index.js
@@ -53,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Empirical variograms",
     "title": "GeoStats.EmpiricalVariogram",
     "category": "Type",
-    "text": "EmpiricalVariogram(X, z, [optional parameters])\n\nComputes the empirical (a.k.a. experimental) omnidirectional (semi-)variogram from data locations X and values z.\n\nEmpiricalVariogram(geodata, var, [optional parameters])\n\nAlternatively, compute the variogram for the variable var stored on a GeoDataFrame object geodata.\n\nParameters\n\nnbins - number of bins (default to 20)\nmaxlag - maximum lag distance (default to maximum lag of data)\ndistance - custom distance function\n\n\n\n"
+    "text": "EmpiricalVariogram(X, z, [optional parameters])\n\nComputes the empirical (a.k.a. experimental) omnidirectional (semi-)variogram from data locations X and values z.\n\nEmpiricalVariogram(spatialdata, var, [optional parameters])\n\nAlternatively, compute the variogram for the variable var stored in a spatialdata object.\n\nParameters\n\nnbins - number of bins (default to 20)\nmaxlag - maximum lag distance (default to maximum lag of data)\ndistance - custom distance function\n\n\n\n"
 },
 
 {
